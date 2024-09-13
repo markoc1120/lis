d8d2014319fcd089f4057afb7c64fc1f7fdc76ba
markoc1120 <markoc1120@gmail.com>
2024-09-13 11:54:15 +0200

    solution

--- src/lis.py
+++ src/lis.py
@@ -4,7 +4,7 @@
 # They *should* be comparable, and it *is* possible to make such
 # an annotation, but it is tricky, and I don't want to confuse you
 # more than strictly necessary.
-from typing import Sequence, Any
+from typing import Any, Sequence
 
 
 def is_increasing(x: Sequence[Any]) -> bool:
@@ -23,7 +23,7 @@ def is_increasing(x: Sequence[Any]) -> bool:
     False
     """
     for i in range(len(x) - 1):
-        if not x[i] < x[i+1]:
+        if not x[i] < x[i + 1]:
             return False
     return True
 
@@ -46,7 +46,19 @@ def longest_increasing_substring(x: Sequence[Any]) -> tuple[int, int]:
     >>> longest_increasing_substring([12, 45, 32, 65, 78, 23, 35, 45, 57])
     (5, 9)
     """
-    # The leftmost empty string is our first best bet
-    best = (0, 0)
-    # FIXME: explore the other possibilities
-    return best
+    if not x:
+        return (0, 0)
+
+    max_len = 1
+    max_start = 0
+    left = 0
+    for right in range(1, len(x)):
+        if x[right] > x[right - 1]:
+            current_len = right - left + 1
+            if current_len > max_len:
+                max_len = current_len
+                max_start = left
+        else:
+            left = right
+
+    return (max_start, max_start + max_len)
